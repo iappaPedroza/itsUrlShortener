2da0fe06212a946dffd92779162840a392fe658e
iappaPedroza <itstecnologia.criativa@gmail.com>
2022-01-24 20:21:55 -0300

    Projeto de Encurtador de URL - Aula Dio - Ph1

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// import { URLController } from './controller/URLController'
+import express, { Request, Response} from 'express'
+// import { MongoConnection } from './database/MongoConnection'
+
+const api = express()
+api.use(express.json())
+
+/* const database = new MongoConnection()
+database.connect()
+
+const urlController = new URLController()
+api.post('/shorten', urlController.shorten)
+api.get('/:hash', urlController.redirect) */
+
+api.get('/teste', (req: Request, res: Response) => {
+    res.json({ success: true})
+} )
+
+api.listen(5000, () => console.log('ITS - Fase 2 - MongoIng - Express listening'))
